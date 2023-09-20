f9cbd0dadbd0b6a30f1474512ac6d76452770d7e
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-09-20 11:40:16 +0200

    1

--- CS/Program.cs
+++ CS/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace CS
 {
+    
+
     internal class Program
     {
+        public const int n = 10; //wielkosc tablicy
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] t1 = { 12, 3545, 343 };
+            int[] t2 = new int[100];
+            int[] t = new int[n];
+            Random l = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                t[i] = l.Next(10,78);
+                Console.Write(t[i] + " ");
+            }
+            
+ 
+        
         }
     }
 }
